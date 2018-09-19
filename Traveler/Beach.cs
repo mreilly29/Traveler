f6449c8e023b2645fbafb9d4e2c3c6b3fbf566f0
mreilly29 <mreilly18@jcu.edu>
2018-09-19 15:31:19 -0400

    initial commit

---
+++ Traveler/Beach.cs
@@ -0,0 +1,63 @@
+﻿namespace TravelerTests
+{
+    public class Beach
+    {
+        //FIELDS
+        private string eyeColor;
+        private double shoeSize;
+
+        //PROPERTIES
+        public string GetColor
+        {
+            get { return this.eyeColor; }
+            set { this.eyeColor = value; }
+        }
+
+        public double GetShoeSize
+        {
+            get { return this.shoeSize; }
+            set { this.shoeSize = value; }
+        }
+
+        //CONSTRUCTOR
+        public Beach(string value)
+        {
+            this.GetColor = value;
+        }
+
+        public Beach() //default constructor
+        {
+
+        }
+
+        //METHODS
+        public string GetBeach(string value)
+        {
+            if (value == "green")
+            {
+                return "Seal Beach";
+            }
+            else if (value == "brown")
+            {
+                return "Newport Beach";
+            }
+            else if (value == "grey")
+            {
+                return "Huntington Beach";
+            }
+            else
+            {
+                return " ";
+            }
+        }
+        
+        public string GetHotel(double num, string value)
+        {
+            if((num == 10) && (value == "Seal Beach"))
+            {
+                return "Hilton";
+            }
+            return null;
+        }
+    }
+}
---
+++ TravelerTests/TravelerTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelerTests
+{
+    [TestFixture]
+    class TravelerTests
+    {
+        Beach beach = new Beach();
+        [Test]
+        public void Enter_Eye_Color()
+        {
+            Beach beach = new Beach("blue");
+            string response = beach.GetColor;
+            Assert.That(response, Is.EqualTo("blue"));
+        }
+
+        [Test]
+        public void Enter_String_Return_Beach()
+        {            
+            string response = beach.GetBeach("blue");
+            Assert.That(response, Is.EqualTo(" "));
+        }
+
+        [Test]
+        public void Enter_String_Return_Beach_Not_Empty()
+        {
+            string response = beach.GetBeach("green");
+            Assert.That(response, Is.EqualTo("Seal Beach"));
+        }
+
+        [Test]
+        public void Enter_String_Return_Beach_Color_Brown()
+        {
+            string response = beach.GetBeach("brown");
+            Assert.That(response, Is.EqualTo("Newport Beach"));
+        }
+
+        [Test]
+        public void Enter_String_Return_Beach_Color_Grey()
+        {
+            string response = beach.GetBeach("grey");
+            Assert.That(response, Is.EqualTo("Huntington Beach"));
+        }
+
+        [Test]
+        public void Set_Shoe_Size()
+        {
+            beach.GetShoeSize = 10.5;
+            double response = beach.GetShoeSize;
+            Assert.That(response, Is.EqualTo(10.5));
+        }
+
+        [Test]
+        public void Get_Hotel_Size_Equals_0_And_Beach_Equals_Empty_Return_Null()
+        {
+            string location = beach.GetBeach(" ");
+            string response = beach.GetHotel(0, location);
+            Assert.That(response, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void Get_Hotel_Size_Equals_10_And_Beach_Equals_Seal_Beach_Return_Hilton()
+        {
+            string location = beach.GetBeach("green");
+            string response = beach.GetHotel(10, location);
+            Assert.That(response, Is.EqualTo("Hilton"));
+        }
+    }
+}
